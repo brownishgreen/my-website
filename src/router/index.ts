65322c6ea9cb41e1c7f6ecbd726a337fbd46a4fa
Jing Yuan WU <jjjjjimyuan@gmail.com>
2025-06-22 22:27:13 +0800

    Fix(router): switch to hash mode to support GitHub Pages routing

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HomePage from '../pages/HomePage.vue'
 import AboutPage from '../pages/AboutPage.vue'
 import Changelog from '../pages/Changelog.vue'
@@ -156,7 +156,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 })
 
