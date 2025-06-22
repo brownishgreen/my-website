f3f9a733c59995ea9837c977daf4e79e12c2c376
Jing Yuan WU <jjjjjimyuan@gmail.com>
2025-06-22 22:17:36 +0800

    Fix: set correct base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import tailwindcss from 'tailwindcss'
 import autoprefixer from 'autoprefixer'
 
 export default defineConfig({
+  base: '/my-website/',
   plugins: [vue()],
   css: {
     postcss: {
