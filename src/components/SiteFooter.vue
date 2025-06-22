cc1fd965ead8b7dddb6e07727edf80cc20f73199
Jing Yuan WU <jjjjjimyuan@gmail.com>
2025-06-22 22:53:38 +0800

    Fix: remove back to top in Footer

--- src/components/SiteFooter.vue
+++ src/components/SiteFooter.vue
@@ -5,13 +5,6 @@
     <div class="max-w-[720px] mx-auto px-4">
       <p class="mb-2">© {{ new Date().getFullYear() }} Yuan WU</p>
       <div class="flex justify-center gap-6 items-center">
-        <!-- Back to Top -->
-        <a
-          href="#top"
-          class="dark:text-white transition-colors w-full text-zinc-700 hover:text-blue-500 hover:scale-105 duration-200 ease-in-out"
-        >
-          Top ↑
-        </a>
       </div>
     </div>
   </footer>
