3f5f6a2184cbaf72b37456137a54eaca2e6b6801
Jing Yuan WU <jjjjjimyuan@gmail.com>
2025-07-28 01:05:10 +0800

    Update: article update

--- src/pages/AboutPage.vue
+++ src/pages/AboutPage.vue
@@ -20,7 +20,7 @@
       <h3 class="text-xl mt-3 mb-3 dark:text-white">關於我</h3>
       <br />
       <p class="dark:text-white">
-        嗨，我是 Yuan。<br /><br />
+        Nihao，我是 Yuan。<br /><br />
         自 2024 年起開始接觸網頁開發，主要使用 JavaScript，涵蓋前後端技術。<br /><br />偏好簡潔的架構，樂於嘗試不同框架與工具。<br /><br />
         此網站紀錄生活、學習、整理想法等用途。<br /><br />
         <!-- 👉 <a href="你的使用說明書連結">個人使用說明書</a> -->
